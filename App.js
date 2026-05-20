21b5422828de86d638c0a58c5a8dffba1810bd03
dino7tt-hub <dino7tt@gmail.com>
2026-05-20 16:11:14 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Text>BTC BOT READY</Text>
+    </View>
+  );
+}
